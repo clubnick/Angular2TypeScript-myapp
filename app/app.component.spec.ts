86d15d543604b5c3ef3003c68d20e75f3c26c461
StanLi <stanislav.klabnik@best-gaming.com>
2017-01-18 17:05:07 +0100

    2.4.0 (2017-01-18) * modify (Unit Test) app.component.spec.ts to avoid "router-outlet" exception according to * https://angular.io/docs/ts/latest/guide/testing.html#!#router-outlet-component

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,7 +1,8 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }           from '@angular/platform-browser';
+import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 describe('AppComponent', function () {
@@ -11,35 +12,36 @@ describe('AppComponent', function () {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      declarations: [AppComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('h1'));
+      });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
-  });
+  it('should be true = true ', () => expect(true).toBe(true));
 
-  it('should be true ', () => expect(true).toBe(true) );
-/*  
+  it('should be false = false ', () => expect(false).toBe(false));
 
-  it('should create component', () => expect(comp).toBeDefined() );
-
-  it('should create component simple syntax', () =>   
-    expect(comp).toBeDefined() 
+  it('should create component simple syntax', () =>
+    expect(comp).toBeDefined()
   )
 
-  it('should create component comples syntax', () =>   {
-    expect(comp).toBeDefined() ;
+  it('should create component comples syntax', () => {
+    expect(comp).toBeDefined();
   });
+
 
   it('should have expected <h1> text', () => {
     fixture.detectChanges();
     const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
+    expect(h1.innerText).toMatch(/angu/i,
       '<h1> should say something about "Angular"');
   });
-*/
+
 });
